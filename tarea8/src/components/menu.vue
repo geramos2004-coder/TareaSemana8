5e6c6465a4433d629af3a78b5cff09b39101f611
noe <mm2021207@inti.edu.sv>
2025-09-16 22:27:17 -0600

    Menu centrado

--- tarea8/src/components/menu.vue
+++ tarea8/src/components/menu.vue
@@ -1,88 +1,76 @@
 <template>
-  <nav class="navbar">
-    <div class="logo">MiLogo</div>
+
+    <nav class="navbar">
     <ul class="nav-links">
       <li><a href="#">Inicio</a></li>
       <li><a href="#">Categorias</a></li>
       <li><a href="#">Servicios</a></li>
       <li><a href="#">Contacto</a></li>
     </ul>
-    <button class="btn-login">Iniciar Sesión</button>
   </nav>
+
+  <!-- <h1>numero:{{ uno }}</h1>-->
+    
 </template>
 
 <script setup>
-let uno = 5;
+
+var uno=5;
+
 </script>
 
 <style scoped>
+
 .navbar {
   display: flex;
   justify-content: space-between;
   align-items: center;
-  background: linear-gradient(90deg, #1f4037, #99f2c8);
-  padding: 1rem 2rem;
-  border-radius: 12px;
-  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.2);
+  background: linear-gradient(90deg, rgb(28, 30, 32), #3c535a);
+  padding: 0.8rem 1.5rem;
+  border-radius: 8px;
+  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
+
+ 
+
+  .nav-links {
+    list-style: none;
+    display: flex;
+    gap: 1.5rem;
+
+    li a {
+      color: white;
+      text-decoration: none;
+      font-weight: 500;
+      position: relative;
+
+      &:hover {
+        color: #ffeb3b;
+      }
+
+      &::after {
+        content: "";
+        position: absolute;
+        width: 0%;
+        height: 2px;
+        background: #ffeb3b;
+        left: 0;
+        bottom: -4px;
+        transition: width 0.3s ease;
+      }
+
+      &:hover::after {
+        width: 50%;
+      }
+    }
+  }
 }
 
-/* Logo */
-.logo {
-  font-size: 1.5rem;
-  font-weight: bold;
-  color: white;
-  letter-spacing: 2px;
+.content {
+  margin-top: 1rem;
+  padding: 1rem;
+  background: #f8f9fa;
+  border-radius: 8px;
+  text-align: center;
 }
 
-/* Links */
-.nav-links {
-  list-style: none;
-  display: flex;
-  gap: 2rem;
-}
-
-.nav-links li a {
-  color: white;
-  text-decoration: none;
-  font-weight: 500;
-  position: relative;
-  transition: color 0.3s ease;
-}
-
-.nav-links li a:hover {
-  color: #ffe600;
-}
-
-.nav-links li a::after {
-  content: "";
-  position: absolute;
-  width: 0%;
-  height: 2px;
-  background: #ffe600;
-  left: 0;
-  bottom: -4px;
-  transition: width 0.3s ease;
-}
-
-.nav-links li a:hover::after {
-  width: 100%;
-}
-
-/* Botón */
-.btn-login {
-  padding: 0.5rem 1rem;
-  background: #ffe600;
-  color: #1f4037;
-  border: none;
-  border-radius: 20px;
-  font-weight: bold;
-  cursor: pointer;
-  transition: all 0.3s ease;
-}
-
-.btn-login:hover {
-  background: white;
-  color: #1f4037;
-  transform: scale(1.05);
-}
 </style>
